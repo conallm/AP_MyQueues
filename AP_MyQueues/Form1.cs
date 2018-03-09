5783e14d87e3d8f800c8daeff74faf37c45ffec5
Conall Morgan <10379455@mydbs.ie>
2018-03-09 16:53:42 +0000

    Add project files.

---
+++ AP_MyQueues/Form1.cs
@@ -0,0 +1,27 @@
+﻿using AP_MyQueues.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AP_MyQueues
+{
+    public partial class Form1 : Form
+    {
+        List<Book> books = new List<Book>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var mine = from b in books where b.Author == "me" select b;
+        }
+    }
+}
---
+++ AP_MyQueues/Properties/MyQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AP_MyQueues.Properties
+{
+    class MyQueue
+    {
+
+            int head = 0, tail = 0, size;
+
+            int[] data;
+            public MyQueue(int si)
+            {
+                size = si;
+                data = new int[size];
+            }
+            public void enqueue(int i)
+            {
+                if ((tail + 1) % size != head)
+                {
+                    data[tail++] = i;
+
+                    tail %= size;
+                }
+                else
+                    throw new IndexOutOfRangeException();
+            }
+            public int dequeue()
+            {
+                head %= size;
+
+                if (head != tail)
+
+                    return data[head++];
+
+                else
+
+                throw new IndexOutOfRangeException();
+
+            }
+
+        }
+    }
+
